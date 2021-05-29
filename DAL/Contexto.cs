1db5f859d7a0fe8d0cf0ade43c7a323e12b9c84e
escarolin <escarolin09@hotmail.com>
2021-05-28 23:25:16 -0400

    Add project files.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Escarolin_P1_APL2.DAL
+{
+    public class Contexto : DbContext
+    {
+       
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = Data\Producto.db");
+        }
+    }
+}
