f111b0fc6ac054a20640fb9c7b811b2d9c63b97a
escarolin <escarolin09@hotmail.com>
2021-06-02 18:43:14 -0400

    Agregando

--- Models/Productos.cs
+++ Models/Productos.cs
@@ -9,6 +9,22 @@ namespace Escarolin_P1_APL2.Models
     public class Productos
 
     {
+        [Key]
+        [Required(ErrorMessage = "El campo esta vacio'ProductoId'")]
+        [Range(0, 100, ErrorMessage = "El campo 'ProductoId' no puede ser 0 o (mayor a 1000).")]
+        public int ProductoId { get; set; }
+        [Required(ErrorMessage = "Agregar una descripcion 'Decripcion'")]
+        [MinLength(4, ErrorMessage = "El Campo 'Descripcion' debe tener por lo menos (4 caracteres).")]
+        [RegularExpression(@"\S(.*)\S", ErrorMessage = "Debe ser un texto.")]
+        public string Descripcion { get; set; }
+        [Required(ErrorMessage = "El campo esta vacio agregar una existencia 'Existencia'")]
+        public decimal Existencia { get; set; }
+
+        [Required(ErrorMessage = "No puede  de estar Vacío el campo 'Costo'")]
+        public decimal Costo { get; set; }
+
+        [Required(ErrorMessage = "No puede de estar Vacío el campo 'ValorInventario'")]
+        public decimal ValorInventario { get; set; }
+
     }
-       
 }
