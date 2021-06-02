46e21580b0b75c118cd66d73e90f1d549550f7e9
escarolin <escarolin09@hotmail.com>
2021-06-02 18:29:49 -0400

    Creando clase producto

--- Models/Productos.cs
+++ Models/Productos.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Escarolin_P1_APL2.Models
 {
-    public class Class
-
-
+    public class Productos
 
     {
     }
+       
 }
