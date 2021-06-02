95ca10ad8051cc3fb6d007530740916edb8b1216
escarolin <escarolin09@hotmail.com>
2021-06-02 19:13:37 -0400

    Agregando la BLL

---
+++ BLL/ProductosBLL.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Escarolin_P1_APL2.DAL;
+using Escarolin_P1_APL2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Escarolin_P1_APL2.BLL
+{
+    public class ProductosBLL
+    {
+        public static bool Existe(int id)
+        {
+            Contexto db = new Contexto();
+            bool encontrado = false;
+
+            try
+            {
+                encontrado = db.Productos.Any(p => p.ProductoId == id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return encontrado;
+        }
+        public static Productos Buscar(int id)
+        {
+            Contexto db = new Contexto();
+            Productos productos;
+
+            try
+            {
+                productos = db.Productos.Find(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+
+            return productos;
+        }
+
+
+        public static bool Insertar(Productos productos)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+
+            try
+            {
+                db.Productos.Add(productos);
+                paso = db.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool Guardar(Productos productos)
+        {
+            if (!Existe(productos.ProductoId))
+                return Insertar(productos);
+            else
+                return Modificar(productos);
+        }
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+
+            try
+            {
+                var producto = db.Productos.Find(id);
+
+                if (producto != null)
+                {
+                    db.Productos.Remove(producto);//remueve la entidad
+                    paso = db.SaveChanges() > 0;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+        }
+
+
+
+
+        public static bool Modificar(Productos productos)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+
+            try
+            {
+                db.Entry(productos).State = EntityState.Modified;
+                paso = db.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+        }
+        public static List<Productos> GetProductos()
+        {
+            List<Productos> lista = new List<Productos>();
+            Contexto db = new Contexto();
+            try
+            {
+                lista = db.Productos.ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return lista;
+        }
+
+        public static List<Productos> GetList(Expression<Func<Productos, bool>> criterio)
+        {
+            List<Productos> lista = new List<Productos>();
+            Contexto db = new Contexto();
+            try
+            {
+
+                lista = db.Productos.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return lista;
+        }
+
+
+    }
+}
