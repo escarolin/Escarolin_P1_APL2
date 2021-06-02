3ecd3d3e7e8c9c75ecff931600a37514e9e1378e
escarolin <escarolin09@hotmail.com>
2021-06-02 18:47:45 -0400

    agregado producto al contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Escarolin_P1_APL2.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,7 @@ namespace Escarolin_P1_APL2.DAL
     public class Contexto : DbContext
     {
        
-
+        public DbSet <Productos> Productos { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite(@"Data Source = Data\Producto.db");
